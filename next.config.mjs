4a192b44048a4abe0f80ff2cbbf5716f7f02e6e4
mehedyPUST <mehedy.pust@gmail.com>
2026-06-04 13:38:52 +0600

    next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: false,
-
-  // HeroUI উপাদানগুলোর স্টাইল এবং ডার্ক মোড প্রপার্টি ঠিক রাখার জন্য এটি অত্যন্ত জরুরি
-  transpilePackages: ["@heroui/react", "@heroui/styles"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
