93cf81f33179b9ff442df31a80b0533050cb6a31
mehedyPUST <mehedy.pust@gmail.com>
2026-06-06 20:12:29 +0600

    new job form

--- src/app/dashboard/recruiter/jobs/page.jsx
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RecruiterJobs = () => {
     return (
         <div>
-            <h2>Recruiter jobs</h2>
+            <h2>Recruiter jobs/ Manage all jobs</h2>
         </div>
     );
 };
